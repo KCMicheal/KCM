1c3436693e514364319bcffb620fa11d83baffd3
KCM <egwunwokekc@gmail.com>
2020-06-17 23:39:06 +0100

    1st Web edition

--- xdcv/php/subscribe.php
+++
@@ -1,9 +0,0 @@
-<?php
-$email = $_POST['staticEmail'];
-$formcontent=" This is a new subscriber: $email \n";
-$recipient = "egwunwokekc@gmail.com";
-$subject = "A new Subscriber";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-?>
