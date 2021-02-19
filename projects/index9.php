6771126939c228d38a7c693d5670fd39715fa04a
KCMicheal <kcmicheal@users.noreply.github.com>
2021-02-19 01:02:32 +0100

    updated mini projects link

--- projects/index9.php
+++ projects/index9.php
@@ -1,40 +1,29 @@
 <?php
-
-
-    $weather = "";
-    $error = "";
-
-    if (array_key_exists('city', $_GET)) {
-
-        $city = str_replace(' ', '', $_GET['city']);
-
-        $file_headers = @get_headers("https://completewebdevelopercourse.com/locations/London".$city);
-        if ($file_headers[0] == 'HTTP/1.1 404 Not found') {
-
-            $error = "That city could not be found!";
-
-        } else {
-
-        $forecastPage = file_get_contents("https://completewebdevelopercourse.com/locations/London".$city);
-
-
-            $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
-
-            if  (sizeof ($pageArray) > 1) {
-                $secondPageArray = explode('</span></span></span></p>', $pageArray[1]);
-
-                if (sizeof($secondPageArray) > 1) {
-                    $weather = $secondPageArray[0];
-                } else {
-
-                    $error = "That city could not be found!";
-                }
-                } else {
-                    $error = "That city could not be found!";
-                }
-             }
-        }
-
+      $weather = "";
+      $error = "";
+      
+      if ($_GET['city']) {
+          
+       $urlContents = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET['city'])."&appid=cf6cd76dd706ff68238c2bcc3f56b3cd");
+          
+          $weatherArray = json_decode($urlContents, true);
+          
+          if ($weatherArray['cod'] == 200) {
+          
+              $weather = "The weather in ".$_GET['city']." is currently '".$weatherArray['weather'][0]['description']."'. ";
+  
+              $tempInCelcius = intval($weatherArray['main']['temp'] - 273);
+  
+              $weather .= " The temperature is ".$tempInCelcius."&deg;C and the wind speed is ".$weatherArray['wind']['speed']."m/s.";
+              
+          } else {
+              
+              $error = "Could not find city - please try again.";
+              
+          }
+          
+      }
+  
 
 ?>
 <!DOCTYPE html>
@@ -43,8 +32,8 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+ 
     <title>Weather Scrapper</title>
     <style type="text/css">
         body {
@@ -56,9 +45,9 @@
         }
         .container {
             text-align: center;
-            margin-top: 200px;
+            margin-top: 25vh;
             color: whitesmoke;
-            width: 500px;
+            max-width: 500px;
         }
         input {
             margin: 20px 0px;
@@ -66,13 +55,18 @@
         #weather {
             margin-top: 15px;
         }
+        @media all and (max-width: 576px) {
+            h1{
+                text-transform: uppercase;
+            }
+        }
 
     </style>
   </head>
 
   <body>
 
-  <div class="container">
+  <div class="container col">
         <h1>What's the Weather</h1>
 
         <form>
@@ -114,7 +108,7 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="bootstrap4/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
   </body>
 </html>
